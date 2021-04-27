89cc66223e9541a8fbf7bacc7d0f4e5a2b47b6ef
mand cashin <mand.cashin@ampersandit.co.uk>
2021-04-27 10:34:58 +0100

    feat: add static form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { FC } from "react";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
- test
+    <div className="container">
+      <h1 className="title">Typescript TODO List</h1>
+      <form className="form">
+        <label htmlFor="task">Task that needs doing:</label>
+        <input type="text" id="task" placeholder="Task..." />
+        <label htmlFor="deadline">Days left to complete:</label>
+        <input type="text" id="deadline" placeholder="No. of days..." />
+        <button>Add to list</button>
+      </form>
+      <div className="list">list goes here</div>
     </div>
   );
-}
+};
 
 export default App;
